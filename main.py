1bd0b20f3e26414a216595db0c6af91defbbfbf8
Patryk Kłosiński <klosu10@gmail.com>
2025-05-08 17:54:26 +0200

    chore: Attempts to parse a transcripts
    Signed-off-by: Patryk Kłosiński <klosu10@gmail.com>

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ Sejm Crawler
 import os
 
 from deputies import download_deputies
+from term_of_office import download_all_terms
 from transcripts import download_transcripts
 
 # Sprawdzenie istnienia folderu 'data'
 if not os.path.exists('data'):
     os.makedirs('data')
+    os.makedirs('data/terms')
     os.makedirs('data/deputies')
     os.makedirs('data/transcripts')
 
 if __name__ == '__main__':
+    download_all_terms()
     download_transcripts()
     download_deputies()
